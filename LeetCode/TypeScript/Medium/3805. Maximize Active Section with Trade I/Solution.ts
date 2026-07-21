d5c4c223907dd4d2ceb129c638b0214a2e7abc74
Sumayea Rahman <shorbonam104@gmail.com>
2026-07-21 22:58:59 +0600

    Add Medium TypeScript Solution from LeetCode

--- LeetCode/TypeScript/Medium/3805. Maximize Active Section with Trade I/Solution.ts
+++ LeetCode/TypeScript/Medium/3805. Maximize Active Section with Trade I/Solution.ts
@@ -1,40 +1,27 @@
 function maxActiveSectionsAfterTrade(s: string): number {
-    const t = "1" + s + "1";
+    let initialOnes = 0;
+    let prevZeroLen = 0;
+    let currZeroLen = 0;
+    let maxDelta = 0;
 
-    const runs: [string, number][] = [];
-
-    let i = 0;
-    while (i < t.length) {
-        let j = i;
-        while (j < t.length && t[j] === t[i]) j++;
-        runs.push([t[i], j - i]);
-        i = j;
-    }
-
-    let ones = 0;
-    for (const [ch, len] of runs) {
-        if (ch === "1") {
-            ones += len;
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '1') {
+            initialOnes++;
+            if (currZeroLen > 0) {
+                if (prevZeroLen > 0) {
+                    maxDelta = Math.max(maxDelta, prevZeroLen + currZeroLen);
+                }
+                prevZeroLen = currZeroLen;
+                currZeroLen = 0;
+            }
+        } else {
+            currZeroLen++;
         }
     }
 
-    // Remove augmented boundary 1s
-    ones -= 2;
-
-    let gain = 0;
-
-    for (let i = 1; i + 1 < runs.length; i++) {
-        if (
-            runs[i][0] === "1" &&
-            runs[i - 1][0] === "0" &&
-            runs[i + 1][0] === "0"
-        ) {
-            gain = Math.max(
-                gain,
-                runs[i - 1][1] + runs[i + 1][1]
-            );
-        }
+    if (currZeroLen > 0 && prevZeroLen > 0) {
+        maxDelta = Math.max(maxDelta, prevZeroLen + currZeroLen);
     }
 
-    return ones + gain;
+    return initialOnes + maxDelta;
 }
