675400ef4e8209a83262e89c4e7c40d52335ea8e
Sumayea Rahman <shorbonam104@gmail.com>
2026-07-23 12:13:02 +0600

    Add TypeScript Solution to Patterns/Bit Manipulation

--- LeetCode/Patterns/Bit Manipulation/3824. Number of Unique XOR Triplets I/Solution.ts
+++ LeetCode/Patterns/Bit Manipulation/3824. Number of Unique XOR Triplets I/Solution.ts
@@ -1,12 +1,6 @@
 function uniqueXorTriplets(nums: number[]): number {
     const n = nums.length;
-
     if (n <= 2) return n;
 
-    let ans = 1;
-    while (ans <= n) {
-        ans <<= 1;
-    }
-
-    return ans;
+    return 1 << (32 - Math.clz32(n));
 }
