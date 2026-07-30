688d7112abb77301a0ec6cdda81cf4fedd74b564
Sumayea Rahman <shorbonam104@gmail.com>
2026-07-30 06:39:02 +0600

    Add Easy TypeScript Solution from LeetCode

--- LeetCode/TypeScript/Easy/3275. Minimum Number of Pushes to Type Word I/Solution.ts
+++ LeetCode/TypeScript/Easy/3275. Minimum Number of Pushes to Type Word I/Solution.ts
@@ -1,8 +1,8 @@
 function minimumPushes(word: string): number {
     const n = word.length;
-    let pushes = 0;
+    let totalPushes = 0;
     for (let i = 0; i < n; i++) {
-        pushes += Math.floor(i / 8) + 1;
+        totalPushes += Math.floor(i / 8) + 1;
     }
-    return pushes;
+    return totalPushes;
 }
