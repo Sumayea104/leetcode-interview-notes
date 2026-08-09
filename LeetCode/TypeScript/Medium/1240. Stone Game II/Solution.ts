bbbd674acad6f23c2e327547c34037fa96e32167
Sumayea Rahman <shorbonam104@gmail.com>
2026-08-09 09:05:01 +0600

    Add Medium TypeScript Solution from LeetCode

--- LeetCode/TypeScript/Medium/1240. Stone Game II/Solution.ts
+++ LeetCode/TypeScript/Medium/1240. Stone Game II/Solution.ts
@@ -1,35 +1,30 @@
 function stoneGameII(piles: number[]): number {
     const n = piles.length;
-
+    
     const suffixSum = new Array(n + 1).fill(0);
     for (let i = n - 1; i >= 0; i--) {
         suffixSum[i] = suffixSum[i + 1] + piles[i];
     }
-    
 
-    const memo: number[][] = Array.from({ length: n }, () => new Array(n + 1).fill(0));
+    const dp: number[][] = Array.from({ length: n + 1 }, () => new Array(n + 1).fill(0));
 
-    function getOptimal(i: number, M: number): number {
+    for (let index = n - 1; index >= 0; index--) {
+        for (let maxTillNow = n; maxTillNow >= 1; maxTillNow--) {
+       
+            if (index + 2 * maxTillNow >= n) {
+                dp[index][maxTillNow] = suffixSum[index];
+                continue;
+            }
 
-        if (i + 2 * M >= n) {
-            return suffixSum[i];
+            for (let X = 1; X <= 2 * maxTillNow && index + X <= n; X++) {
+                const nextM = Math.max(maxTillNow, X);
+                dp[index][maxTillNow] = Math.max(
+                    dp[index][maxTillNow],
+                    suffixSum[index] - dp[index + X][nextM]
+                );
+            }
         }
-        
-        if (memo[i][M] > 0) {
-            return memo[i][M];
-        }
-
-        let minOpponentStones = Infinity;
-
-        for (let X = 1; X <= 2 * M; X++) {
-            const nextM = Math.max(M, X);
-
-            minOpponentStones = Math.min(minOpponentStones, getOptimal(i + X, nextM));
-        }
-
-        memo[i][M] = suffixSum[i] - minOpponentStones;
-        return memo[i][M];
     }
 
-    return getOptimal(0, 1);
+    return dp[0][1];
 }
