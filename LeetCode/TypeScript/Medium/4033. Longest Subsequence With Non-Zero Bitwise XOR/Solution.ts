7b831a3aed5e0f49cc6a96163ea975345f08c0fb
Sumayea Rahman <shorbonam104@gmail.com>
2026-08-15 09:25:48 +0600

    Add Medium TypeScript Solution from LeetCode

---
+++ LeetCode/TypeScript/Medium/4033. Longest Subsequence With Non-Zero Bitwise XOR/Solution.ts
@@ -0,0 +1,21 @@
+function longestSubsequence(nums: number[]): number {
+    let totalXor = 0;
+    let hasNonZero = false;
+
+    for (const num of nums) {
+        totalXor ^= num;
+        if (num !== 0) {
+            hasNonZero = true;
+        }
+    }
+
+    if (!hasNonZero) {
+        return 0;
+    }
+
+    if (totalXor !== 0) {
+        return nums.length;
+    }
+
+    return nums.length - 1;
+}
