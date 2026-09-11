990275306c381242c79673fb4c84e2f406f5281b
Sumayea Rahman <shorbonam104@gmail.com>
2026-09-11 11:29:01 +0600

    Add Easy Python Solution for 3799. Unique 3-Digit Even Numbers from LeetCode

--- LeetCode/Python/Easy/3799. Unique 3-Digit Even Numbers/README.md
+++ LeetCode/Python/Easy/3799. Unique 3-Digit Even Numbers/README.md
@@ -8,8 +8,8 @@
 Array, Hash Table, Recursion, Enumeration
 
 ### 🚀 Performance
-- **Runtime:** 555 ms
-- **Memory:** 19.3 MB
+- **Runtime:** N/A
+- **Memory:** N/A
 
 ---
 
--- LeetCode/Python/Easy/3799. Unique 3-Digit Even Numbers/Solution.py
+++ LeetCode/Python/Easy/3799. Unique 3-Digit Even Numbers/Solution.py
@@ -1,17 +1,27 @@
-from collections import Counter
 from typing import List
+
 
 class Solution:
 
   def totalNumbers(self, digits: List[int]) -> int:
-    counts = Counter(digits)
+    counts = [0] * 10
+    for d in digits:
+      counts[d] += 1
+
     total = 0
-
     for num in range(100, 1000, 2):
-      d1, d2, d3 = num // 100, (num // 10) % 10, num % 10
-      num_counts = Counter([d1, d2, d3])
+      d1 = num // 100
+      d2 = (num // 10) % 10
+      d3 = num % 10
 
-      if all(counts[d] >= num_counts[d] for d in num_counts):
+      counts[d1] -= 1
+      counts[d2] -= 1
+      counts[d3] -= 1
+
+      if counts[d1] >= 0 and counts[d2] >= 0 and counts[d3] >= 0:
         total += 1
+      counts[d1] += 1
+      counts[d2] += 1
+      counts[d3] += 1
 
     return total
