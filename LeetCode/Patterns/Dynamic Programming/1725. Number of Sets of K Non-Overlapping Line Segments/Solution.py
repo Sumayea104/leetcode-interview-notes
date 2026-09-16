99c2feb8690216cec915d586303458884ad52edc
Sumayea Rahman <shorbonam104@gmail.com>
2026-09-16 14:17:26 +0600

    Add Medium Python Solution for 1725. Number of Sets of K Non-Overlapping Line Segments from LeetCode

--- LeetCode/Patterns/Dynamic Programming/1725. Number of Sets of K Non-Overlapping Line Segments/Solution.py
+++ LeetCode/Patterns/Dynamic Programming/1725. Number of Sets of K Non-Overlapping Line Segments/Solution.py
@@ -1,5 +1,8 @@
 import math
 
+MOD = 10**9 + 7
+comb = math.comb
+
 class Solution:
     def numberOfSets(self, n: int, k: int) -> int:
-        return math.comb(n + k - 1, 2 * k) % (10**9 + 7)
+        return comb(n + k - 1, 2 * k) % MOD
